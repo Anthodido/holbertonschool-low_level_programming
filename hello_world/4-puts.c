eca8a7f0a0c5393e0f3feaa8f9e58f711c852df7
Anthony <anthony.didomenico678@gmail.com>
2025-10-20 13:42:16 +0000

    Ex4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main = Entry point
- *return = succes
+ * main - entry point
  *
+ * Return: always 0 (success)
  */
-Int main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
