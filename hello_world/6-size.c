134aca854ca01a50f1038ac043061e240ccae70c
root <root@ip-10-42-132-35.ec2.internal>
2025-10-21 08:15:09 +0000

    ok

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *Main - entry
+ *return: success
+ *
+ */
+int main(void)
+{  
+printf("size of a char %d byte(s)%d\n",sizeof(char));
+printf("size an int %d bytes(s)%d\n",sizeof(int));
+printf("size of a long int %d byte(s)%d\n",sizeof(long int));
+printf("size of a long long int %d byte(s)%d\n",sizeof(long long int));
+printf("size of a float %d byte(s)%d\n",sizeof(float));
+
+return (0);
+}
