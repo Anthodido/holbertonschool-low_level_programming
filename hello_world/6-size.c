9e68ce2187548208360462036105a983320c4116
root <root@ip-10-42-132-35.ec2.internal>
2025-10-21 08:22:03 +0000

    ok

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *Main - entry
- *return: success
+ * main - Entry od the program
+ * Return: value 0 (Success)
  *
  */
 int main(void)
-{  
+{
 printf("size of a char %d byte(s)%d\n", sizeof(char));
 printf("size an int %d bytes(s)%d\n", sizeof(int));
 printf("size of a long int %d byte(s)%d\n", sizeof(long int));
