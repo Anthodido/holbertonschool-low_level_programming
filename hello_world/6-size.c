4791737fd9e4b6a119c11b914c6d91751c429318
root <root@ip-10-42-132-35.ec2.internal>
2025-10-21 08:18:28 +0000

    ok

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,6 +11,5 @@ printf("size an int %d bytes(s)%d\n", sizeof(int));
 printf("size of a long int %d byte(s)%d\n", sizeof(long int));
 printf("size of a long long int %d byte(s)%d\n", sizeof(long long int));
 printf("size of a float %d byte(s)%d\n", sizeof(float));
-
 return (0);
 }
