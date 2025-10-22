707aa47514aadcd55641948d51051b8af1aa4b60
root <root@ip-10-42-177-48.ec2.internal>
2025-10-22 10:19:07 +0000

    .

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ char alphaa;
 char alphaA;
 for (alphaa = 'a'; alphaa <= 'z'; alphaa++)
 for (alphaA = 'A'; alphaA <= 'Z'; alphaA++)
-putchar(alpha);
+putchar(alphaa);
 putchar(alphaA);
 putchar('\n');
 
