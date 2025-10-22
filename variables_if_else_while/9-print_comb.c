1763e3fe7fdc379e7cec14e0074831216a99f5c3
root <root@ip-10-42-177-48.ec2.internal>
2025-10-22 13:31:37 +0000

    .

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+int p;
+for (p = '0'; p <= '9'; p++)
+{
+putchar (p);
+if (p != 9)
+putchar (',');
+putchar (' ');
+}
+putchar ('\n');
+return (0);
+}
