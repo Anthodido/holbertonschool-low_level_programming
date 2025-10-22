e1ba9ef44ab9fbcf20be558f4e4c00cd9cd85564
root <root@ip-10-42-122-34.ec2.internal>
2025-10-22 09:09:35 +0000

    .

---
+++ variables_if_else_while/ 2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *Main - Entry
+ *return: 0 (success)
+ *
+ */
+
+int main(void);
+{
+char alpha;
+
+for (alpha = 'a' ; alpha <= 'z'; alpha++ )
+putchar(alpha);
+putchar('\n');
+return (0);
+}
