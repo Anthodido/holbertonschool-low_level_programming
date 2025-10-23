9c83b605b5a8452486ff72c5a3356315e992a24c
root <root@ip-10-42-199-117.ec2.internal>
2025-10-23 00:09:19 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return (0);
+int main() {
+printf("_putchar\n");
+return 0;
 }
