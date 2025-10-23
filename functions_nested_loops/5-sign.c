00dc7101e1d35b9804b182bf21c29e31c749adcc
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 11:56:25 +0000

    .

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,1 +1,25 @@
 #include "main.h"
+/**
+ *print_sign -vérifie le signe du nombre
+ *@n: est le caraactère à vérifier
+ *Return: soit + soit - soit 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar (+);
+return (1);
+}
+else if (n == 0)
+{
+_putchar (0);
+return (0);
+}
+else
+{
+_putchar (-);
+return (-1);
+}
+}
