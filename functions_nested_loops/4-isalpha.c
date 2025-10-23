f95835c0c7457e5f4c51cef00848f3c599fca443
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 11:39:33 +0000

    .

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,15 @@
 #include "main.h"
 /**
- *_islower -Verifie si le caractere est en minuscule
+ *isalpha -Verifie si le caractere est en minuscule ou en majuscule
  *@c: est le caractère à vérifier
  *Return: 1 if it's  lowercase else 0
  */
 
 int isalpha(int c)
 {
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+
+else
+return (0);
+}
