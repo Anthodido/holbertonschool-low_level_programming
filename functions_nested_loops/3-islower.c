84f6f7640662fe4e130d5e449faa162d6cc0bceb
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 11:27:01 +0000

    .

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_islower -Verifie si le caractere est en minuscule
+ *@c: est le caractère à vérifier
+ *return: 1 if it's  lowercase else 0
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+
+else
+return (0);
+}
