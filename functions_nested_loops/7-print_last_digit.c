ca6c539c87e6c4c083c7271ee84076ecca0670b9
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 13:57:36 +0000

    .

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,3 +4,18 @@
  *
  *
  */
+int print_last_digit(int c)
+{
+int last_digit;
+last_digit = c % 10
+
+if (last_digit < 0)
+last_digit = -last_digit;
+
+_putchar (last_digit + '0');
+
+else
+_putchar (last_digit);
+
+return (last_digit);
+}
