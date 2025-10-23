741d82ce0897020f1910446ee9dfec14ccacc764
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 11:31:09 +0000

    .

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *_islower -Verifie si le caractere est en minuscule
+ *@c: est le caractère à vérifier
+ *Return: 1 if it's  lowercase else 0
+ */
+
+int _islower(int c)
+{
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c); /**je déclare la fonction _putchar*/
 int _islower(int c);
+int _isalpha(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 
