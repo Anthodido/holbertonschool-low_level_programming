784d5f5b515cc0d12d39e6a377710c58d187b6d7
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 09:52:10 +0000

    .

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
-
+/**
+* print_alphabet_x10 - print alphabet 10 times
+*
+*/
 void print_alphabet_x10(void)
 {
 int i;
 char x;
 
-for(i = 0; i < 10; i++);
-for (x = 'a'; x <= 'z'; x++);
-
+for (i = 0; i < 10; i++)
+{
+for (x = 'a'; x <= 'z'; x++)
+{
 _putchar (x);
+}
 _putchar ('\n');
-
+}
 
 }
