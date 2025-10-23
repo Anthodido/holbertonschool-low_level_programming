b85a56a3fbcca1b437332e73714d208c5d7036e5
root <root@ip-10-42-14-185.ec2.internal>
2025-10-23 11:28:35 +0000

    .

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  *_islower -Verifie si le caractere est en minuscule
  *@c: est le caractère à vérifier
- *return: 1 if it's  lowercase else 0
+ *Return: 1 if it's  lowercase else 0
  */
 
 int _islower(int c)
