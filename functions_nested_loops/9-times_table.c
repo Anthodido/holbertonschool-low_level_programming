d6ac1edfc080f06d73d46fee3ff59a71a9bb07c8
root <root@ip-10-42-250-222.ec2.internal>
2025-10-24 09:44:49 +0000

    .

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,4 +4,35 @@
  *
  *Return: (success)
  */
- 
+ void times_table(void)
+ {
+int d, m, r;
+
+for (d = 0; d <= 9; d++)
+{
+for (m = 0; m <= 9; m++)
+{
+r = d * m;
+
+if (r < 10)
+{
+if (r != 0)
+_putchar (' ');
+_putchar (r + '0');
+}
+
+else
+{
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
+}
+
+if (r != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
