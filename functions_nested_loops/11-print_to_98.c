3ed9226b3ebfa7a85b203e5384da709e2c9b9b0e
root <root@ip-10-42-250-222.ec2.internal>
2025-10-24 12:08:38 +0000

    .

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*
-*
+*print_to_98 - Ecris les nombres de n à 98
+*@n : la variable qui va débuter
 *
 */
 void print_to_98(int n)
 {
-    if (n <=98)
-    {
-    for (; n <= 98; n++)
-    {
-        printf("%d" , n);
-        if (n != 98)
-        printf(", ");
-    }
-    }
-    else
-    {
-        for (;n >= 98; n--)
-        {
-            printf("%d", n);
-            if (n != 98)
-            printf (", ");
-        }
-    }
-    printf("\n");
-
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+printf("%d", n);
+if (n != 98)
+printf(", ");
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+printf("%d", n);
+if (n != 98)
+printf(", ");
+}
+}
+printf("\n");
 }
