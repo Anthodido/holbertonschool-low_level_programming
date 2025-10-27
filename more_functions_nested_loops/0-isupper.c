efe8f94d2f50dffcf6994a430048944be3954dae
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 09:58:48 +0000

    .

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _isupper - Créer la variable c
+ * je parcours l'alphabet et dis si il y a des caractères majuscule
+ * Return: (1) si la variable c majuscule sinon (0)
+ * @c: variale
+ */
 
 int _isupper(int c)
 {
