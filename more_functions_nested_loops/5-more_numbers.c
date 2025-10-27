e968153369a7bcf93e873041749cae926616f860
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 11:59:10 +0000

    .

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * print_most_numbers - rien
+ * more_numbers - rien
  *
  *
  */
@@ -10,7 +10,7 @@ void more_numbers(void)
 char mlt, n;
 for (n = '0'; n <= '10'; n++)
 for (mlt = '0'; mlt <= '14'; mlt++)
-if (n > '9');
+if (n > '9')
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 
