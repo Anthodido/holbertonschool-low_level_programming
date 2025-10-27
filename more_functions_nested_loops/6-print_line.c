bb8a3f68c23abb62f949f2d09c9a3af451fa3d83
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 13:25:06 +0000

    .

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,13 @@
 #include "main.h"
 /**
+ * print_line - Dessine une ligne droite dans le terminal
+ * @n: Le nombre de fois que le caractère '_' doit être imprimé
  *
+ * Description: Si n est inférieur ou égal à 0, la fonction
+ * n’affiche qu’un retour à la ligne. Sinon, elle affiche n
+ * fois le caractère '_' suivi d’un retour à la ligne.
  *
- *
+ * Return: Rien (void)
  */
 
 void print_line(int n)
@@ -10,11 +15,11 @@ void print_line(int n)
 int l;
 
 if (n <= 0)
-_putchar ('\n');
-
-else
-{ 
+{
+_putchar('\n');
+return;
+}
 for (l = 0; l < n; l++)
 _putchar ('_');
-}
+_putchar ('\n');
 }
