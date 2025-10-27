b622c9b06670ca453e52a5773ee368db567c5efd
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 10:18:09 +0000

    .

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Créer deux variables
+ * je multiplie a par b
+ * Return: (a * b)
+ * @a: variale
+ * @b: variable
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int print_last_digit(int);
 int add(int, int);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
