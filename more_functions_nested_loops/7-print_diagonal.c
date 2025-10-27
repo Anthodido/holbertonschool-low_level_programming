86d88397b6456ab290d2456f055e6404e1ff376d
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 13:53:19 +0000

    .

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - Dessine une ligne diagonale dans le terminal
+ * @n: Le nombre de fois que le caractère '\' doit être imprimé
+ *
+ * Description: Si n est inférieur ou égal à 0, la fonction affiche
+ * simplement un retour à la ligne. Sinon, elle affiche une diagonale
+ * formée par le caractère '\' avec des espaces croissants avant chaque ligne.
+ *
+ * Return: Rien (void)
+ */
+
+void print_line(int n)
+{
+int h, v;
+
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+for (h = 0; h < n; h++)
+for (v = 0; v <= h; v++)
+_putchar (' ');
+_putchar ('\\');
+_putchar ('\n');
+}
