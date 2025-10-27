6431b679ba136c67a19df806b6a95174105c6336
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 11:24:41 +0000

    .

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include'main.h'
+/**
+ * print_most_numbers - rien
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+char exc
+for (exc = '0'; exc <= '9'; exc++)
+
+if (exc '2' && '4')
+_putchar (exc);
+}
