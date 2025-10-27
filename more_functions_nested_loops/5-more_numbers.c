cbe62deb891e648729891236357c57824e1e938a
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 12:14:42 +0000

    Ex5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,15 @@
 void more_numbers(void)
 {
 char mlt, n;
+
 for (mlt = '0'; mlt < 10; mlt++)
+{
 for (n = '0'; n <= 14; n++)
+{
 if (n > 9)
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
-
+}
 _putchar ('\n');
-
+}
 }
