bb2758985cc998d5986cd1577601429cd268a37d
root <root@ip-10-42-94-107.ec2.internal>
2025-10-27 12:05:50 +0000

    .

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 char mlt, n;
 for (n = '0'; n <= 10; n++)
 for (mlt = '0'; mlt <= 14; mlt++)
-if (n > '9')
+if (n > 10)
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 
