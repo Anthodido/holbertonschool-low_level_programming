a33f63bcf352a62356f4dff1f9506d1f43cc7e4c
root <root@ip-10-42-117-0.ec2.internal>
2025-10-28 08:37:51 +0000

    .

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int add(int, int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_square(int size);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
