62edf3c79fc83a36b3b644c909ed787e1350d472
root <root@ip-10-42-117-0.ec2.internal>
2025-10-28 10:05:32 +0000

    .

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,20 @@ int main (void)
 int n;
 {
 for (n = 1; n <= 100; n++)
-putchar ('\n');
+printf('\n');
 }
-if (n % 3 == 0)
-printf("Fizz");
-
-if (n % 5 == 0)
-printf("Buzz");
-
 if (n % 5 == 0 && n % 3 == 0)
 printf("FizzBuzz");
 
-putchar (' ');
+else if (n % 3 == 0)
+printf("Fizz");
+
+else if (n % 5 == 0)
+printf("Buzz");
+
+else (n != 100)
+printf(' ');
+
+
 return (0);
 }
