ac0f26384a1a9dda3d070fa421d7a9ce3f14ae8f
root <root@ip-10-42-117-0.ec2.internal>
2025-10-28 09:51:07 +0000

    .

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints numbers from 1 to 100, followed by a new line.
+ *        For multiples of three, prints "Fizz" instead of the number,
+ *        for multiples of five, prints "Buzz",
+ *        and for multiples of both three and five, prints "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+
+{
+int n;
+{
+for (n = 1; n <= 100; n++)
+_putchar ('\n');
+}
+if (n % 3 == 0)
+printf("Fizz");
+
+if (n % 5 == 0)
+printf("Buzz");
+
+if (n % 5 == 0 && n % 3 == 0)
+printf("FizzBuzz");
+
+_putchar (' ');
+return (0);
+}
