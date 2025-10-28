487f23c59819966dc4de4c9a97f7cd846ccf172c
root <root@ip-10-42-117-0.ec2.internal>
2025-10-28 11:01:35 +0000

    .

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - imprime un triangle de '#' selon la taille donnée
+ * @size: taille du triangle
+ *
+ * Description: Cette fonction affiche un triangle aligné à droite
+ * utilisant le caractère '#' et une taille spécifiée par l'utilisateur.
+ * Si size <= 0, la fonction affiche simplement une nouvelle ligne.
+ */
+
+void print_triangle(int size)
+{
+int L, l;
+
+if (size <= 0)
+{
+_putchar ('\n');
+return;
+}
+for (L = 0; L <= size; L++)
+{
+for (l = 0; l <= size - L; l++)
+_putchar(' ');
+
+for (l = 0; l < L; l++)
+_putchar('#');
+_putchar('\n');
+}
+}
