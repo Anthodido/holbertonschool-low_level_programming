ab0c0a52bb05dca9f1afd3152e99149c2ceb7298
root <root@ip-10-42-8-92.ec2.internal>
2025-10-29 08:11:19 +0000

    .

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - met à jour la valeur d'un entier pointé à 98
+ * @n: pointeur vers l'entier à modifier
+ *
+ * Description: Cette fonction prend un pointeur vers un entier en paramètre
+ * et modifie la valeur stockée à cette adresse pour la remplacer par 98.
+ */
+
+void reset_to_98(int *n)
+
+{
+
+*n = 98;
+
+}
