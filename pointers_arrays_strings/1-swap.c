60f85daafb72f0594869db74fb18bcfa4d252b1d
root <root@ip-10-42-8-92.ec2.internal>
2025-10-29 09:19:21 +0000

    .

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,6 +1,13 @@
 #include "main.h"
-
-
+/**
+ * swap_int - échange les valeurs de deux entiers
+ * @a: pointeur vers le premier entier
+ * @b: pointeur vers le deuxième entier
+ *
+ * Description: Cette fonction prend deux pointeurs vers des entiers et
+ * échange leurs valeurs en utilisant une variable temporaire.
+ */
+ 
 void swap_int(int *a, int *b)
 
 {
