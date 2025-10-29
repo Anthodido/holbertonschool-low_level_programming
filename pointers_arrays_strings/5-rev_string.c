d311ea2085b94fcf8096548ae691fb7ab609d097
root <root@ip-10-42-130-76.ec2.internal>
2025-10-29 19:58:04 +0000

    .

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - affiche une chaîne de caractères à l’envers
+ * @s: pointeur vers la chaîne de caractères à afficher
+ *
+ * Description: Parcourt la chaîne jusqu’à la fin, puis affiche
+ * chaque caractère dans l’ordre inverse à l’aide de _putchar.
+ */
+void rev_string(char *s)
+{
+int f = 0;
+int l;
+
+while (s[f] != '\0')
+{
+f++;
+}
+for (l = f - 1; l >= 0; l--)
+{
+_putchar (s[l]);
+}
+_putchar ('\n');
+}
