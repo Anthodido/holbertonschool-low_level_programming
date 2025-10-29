2e374968670d44720773579e7454b604ffc50e34
root <root@ip-10-42-8-92.ec2.internal>
2025-10-29 12:48:47 +0000

    .

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 
-void print_rev(char *s);
+void print_rev(char *s)
+
+{
+int f = 0;
+int h;
+
+while (s[f] != '\0')
+f ++;
+
+for (h = f - 1; h >= 0; h--)
+_putchar (s[h]);
+
+_putchar ('\n');
+
+}
