a5fabccfd0f78f0378026119a6a3d4f0e706617d
root <root@ip-10-42-8-92.ec2.internal>
2025-10-29 10:22:45 +0000

    .

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,21 @@
 #include "main.h"
-
+/**
+ * _strlen - retourne la longueur d'une chaîne de caractères
+ * @s: pointeur vers la chaîne dont on veut connaître la longueur
+ *
+ * Description: Parcourt la chaîne pointée par @s jusqu'au caractère nul
+ * ('\0') et compte le nombre de caractères présents.
+ *
+ * Return: la longueur de la chaîne (nombre de caractères avant '\0')
+ */
 
 int _strlen(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+return (len);
+
+}
