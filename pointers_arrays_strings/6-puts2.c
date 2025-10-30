705da7ebdb291ae4821693fa3b18748a23caa832
root <root@ip-10-42-170-165.ec2.internal>
2025-10-30 12:14:53 +0000

    .

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,14 @@
 #include "main.h"
-
-
-
+/**
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: Pointer to the string to print
+ *
+ * Description: This function prints one character out of two from the string,
+ * starting with the first character, followed by a new line.
+ * Return: Nothing (void)
+ */
 void puts2(char *str)
-
+{
 int f = 0;
 {
 while (str[f] != '\0')
@@ -12,4 +17,5 @@ if (f % 2 != 0)
 _putchar (str[f]);
 }
 _putchar ('\n');
+}
 }
