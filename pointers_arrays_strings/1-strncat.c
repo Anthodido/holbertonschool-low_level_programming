5095a7c1af062c6fc6fe0d05e721f5a1c115124b
root <root@ip-10-42-246-37.ec2.internal>
2025-10-31 11:23:39 +0000

    .

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
-
+/**
+ * _strncat - concatenates two strings, using at most n bytes from src
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to the resulting string dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
