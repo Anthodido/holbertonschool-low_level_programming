0bbdc71156a492a0f225239c07bdcc242ad29a84
root <root@ip-10-42-246-37.ec2.internal>
2025-10-31 09:51:15 +0000

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ int _abs(int);
 int print_last_digit(int);
 int add(int, int);
 int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
