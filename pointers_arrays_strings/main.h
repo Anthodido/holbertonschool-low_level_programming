801f397818d7f26bbc01ff9bfd0caf805cfad155
root <root@ip-10-42-58-253.ec2.internal>
2025-10-31 13:07:57 +0000

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ int add(int, int);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
+char *cap_string(char *);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
