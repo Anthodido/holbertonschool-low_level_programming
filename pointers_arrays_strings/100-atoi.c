1e1a8ef9b9a19f9acde98f89e93a11df48aa32a9
root <root@ip-10-42-58-253.ec2.internal>
2025-10-31 17:29:20 +0000

    ex 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,18 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - converts a string to an integer safely
  * @s: string to convert
  *
- * Return: the integer value, or 0 if no numbers found
+ * Return: integer value (clamped to INT_MAX / INT_MIN if overflow)
  */
 int _atoi(char *s)
 {
 int i = 0;
 int sign = 1;
 int num = 0;
-int started = 0;
+int digit;
 
 while (s[i] != '\0')
 {
@@ -19,10 +20,19 @@ if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-started = 1;
-num = (num * 10) + (s[i] - '0');
+digit = s[i] - '0';
+
+if (num > (INT_MAX - digit) / 10)
+{
+if (sign == 1)
+return (INT_MAX);
+else
+return (INT_MIN);
 }
-else if (started)
+
+num = num * 10 + digit;
+}
+else if (num > 0)
 break;
 i++;
 }
