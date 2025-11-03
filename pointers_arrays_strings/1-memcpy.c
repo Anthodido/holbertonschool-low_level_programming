0273bb190d5fd6386621966419356364a9d05457
root <root@ip-10-42-245-85.ec2.internal>
2025-11-03 15:17:36 +0000

    .

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _memcpy - copie une zone mémoire vers une autre
+ * @dest: pointeur vers la zone de destination
+ * @src: pointeur vers la zone source
+ * @n: nombre d’octets à copier
+ *
+ * Return: un pointeur vers dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i = 0;
