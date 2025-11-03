f1c91ec9482bc80275ecc8888324a9e0c2caa9f1
root <root@ip-10-42-245-85.ec2.internal>
2025-11-03 11:53:56 +0000

    .

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,42 @@
 #include "main.h"
 
+/**
+ * _strspn - calcule la longueur du préfixe d'une chaîne
+ * composé uniquement de caractères présents dans une autre chaîne
+ * @s: chaîne à analyser
+ * @accept: chaîne contenant les caractères autorisés
+ *
+ * Return: le nombre d’octets initiaux de s
+ * composés uniquement de caractères présents dans accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 int i = 0;
-int h = 0;
+int h;
 int count = 0;
-while (s[i] != '\0')
-i++;
-{
-while (accept[h] != '\0')
-h++;
+int found;
 
+while (s[i] != '\0')
+{
+found = 0;
+h = 0;
+
+while (accept[h] != '\0')
+{
 if (accept[h] == s[i])
 {
-count++;
+found = 1;
 break;
 }
-if (accept[h] == '\0')
-return (count);
+h++;
 }
+
+if (!found)
+break;
+
+count++;
+i++;
+}
+
 return (count);
 }
