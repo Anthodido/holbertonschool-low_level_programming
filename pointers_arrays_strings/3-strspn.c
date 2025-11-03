24da32c8e9ab36a8d60dbe68b06ffa4056d63e57
root <root@ip-10-42-245-85.ec2.internal>
2025-11-03 11:14:01 +0000

    .

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,6 +2,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+int i = 0;
+int h = 0;
+int count = 0;
+while (s[i] != '\0')
+i++;
+{
+while (accept[h] != '\0')
+h++;
 
-    
+if (accept[h] == s[i])
+{
+count++;
+break;
+}
+if (accept[h] == '\0')
+return (count);
+}
+return (count);
 }
