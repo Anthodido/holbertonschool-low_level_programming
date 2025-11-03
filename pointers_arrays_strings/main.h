71dfa526ccddc32e7082c5217cdd72e51a719759
root <root@ip-10-42-245-85.ec2.internal>
2025-11-03 14:57:24 +0000

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,6 +24,7 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
+void print_chessboard(char (*a)[8]);
 void reverse_array(int *a, int n);
 void print_array(int *a, int n);
 void puts_half(char *str);
