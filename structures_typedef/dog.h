2c2cabdda8ff717beaf07ca9edacfb1dca7b673a
root <root@ip-10-42-187-29.ec2.internal>
2025-11-12 23:00:21 +0000

    .

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Représente un chien.
+ * @name: Nom du chien.
+ * @age: Âge du chien.
+ * @owner: Propriétaire du chien.
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif
