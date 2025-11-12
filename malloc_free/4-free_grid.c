a9f37a2a33d006c69e69dd70af4df1182c37741a
root <root@ip-10-42-114-86.ec2.internal>
2025-11-12 09:39:12 +0000

    .

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * free_grid - frees a 2D grid previously created by alloc_grid
+ * @grid: pointer to the 2D grid to free
+ * @height: height of the grid
+ *
+ * Return: nothing
+ */
 void free_grid(int **grid, int height)
 {
 int i;
