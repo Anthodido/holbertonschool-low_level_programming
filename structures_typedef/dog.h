b187e74b767ceb5b80376aeec66bd334f154e1de
root <root@ip-10-42-52-222.ec2.internal>
2025-11-13 11:54:10 +0000

    .

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: Dog's age.
  * @owner: Dog's owner.
  */
-typedef struct dog, dog_t
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
