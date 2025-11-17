949302bd3f3e7f430880b62848cfabc18421f474
root <root@ip-10-42-104-148.ec2.internal>
2025-11-17 09:25:22 +0000

    .

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: pointer to a function that takes a char * and returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (f == NULL || name == NULL)
+{
+return;
+}
+f(name);
+}
