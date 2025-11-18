efae316d9d82036d94f9c2b05c6d064ffd8c6384
root <root@ip-10-42-2-128.ec2.internal>
2025-11-18 09:38:27 +0000

    .

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,6 +11,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 int sum = 0;
 unsigned int i;
+
 va_list ap;
 
 if (n == 0)
@@ -18,10 +19,12 @@ if (n == 0)
 return (0);
 }
 va_start(ap, n);
+
 for (i = 0; i < n; i++)
 {
 sum += va_arg(ap, int);
 }
 va_end(ap);
+
 return (sum);
 }
