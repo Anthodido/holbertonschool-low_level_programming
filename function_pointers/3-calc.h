22674c6e87c222ce54863be001b9f88f231566df
root <root@ip-10-42-239-255.ec2.internal>
2025-11-21 09:19:27 +0000

    .

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -12,8 +12,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -24,5 +24,4 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 
-
-#endif 
+#endif
