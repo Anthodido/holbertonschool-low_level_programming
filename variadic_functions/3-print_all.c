7bcd1d171be604f8f5235cc3750b3a780d7272b9
root <root@ip-10-42-239-255.ec2.internal>
2025-11-21 09:29:17 +0000

    .

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,25 +3,37 @@
 #include <stdio.h>
 
 typedef void (*printer_f)(va_list *ap);
-
+/**
+ * print_char - prints a char from a va_list
+ * @ap: pointer to the argument list
+ */
 static void print_char(va_list *ap)
 {
 int c = va_arg(*ap, int);
 printf("%c", c);
 }
-
+/**
+ * print_int - prints an int from a va_list
+ * @ap: pointer to the argument list
+ */
 static void print_int(va_list *ap)
 {
 int n = va_arg(*ap, int);
 printf("%d", n);
 }
-
+/**
+ * print_float - prints a float from a va_list
+ * @ap: pointer to the argument list
+ */
 static void print_float(va_list *ap)
 {
 double f = va_arg(*ap, double);
 printf("%f", f);
 }
-
+/**
+ * print_string - prints a string from a va_list
+ * @ap: pointer to the argument list
+ */
 static void print_string(va_list *ap)
 {
 char *s = va_arg(*ap, char *);
