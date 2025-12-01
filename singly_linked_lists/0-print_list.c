a060cccfcee862e558508b90f42cf52fcf27a046
root <root@ip-10-42-54-169.ec2.internal>
2025-12-01 10:59:03 +0000

    .

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,27 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * print_list - Affiche tous les éléments d'une liste de type list_t.
- * @h: Pointeur constant vers le premier élément (tête) de la liste chaînée
- *
- * Return: Le nombre de nœuds présents dans la liste
- */
+* print_list - Prints all the elements of list_t.
+* @h: The list_t list.
+*
+* Return: Number of nodes in h.
+*/
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+    size_t count = 0;
 
-
-while (h != NULL)
+    while (h != NULL)
     {
         if (h->str == NULL)
-        printf("[0] (nil)\n");
-
+            printf("[0] (nil)\n");
         else
             printf("[%d] %s\n", h->len, h->str);
 
         count++;
         h = h->next;
     }
+
     return (count);
 }
