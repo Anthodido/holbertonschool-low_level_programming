bd2b0b0732e17cc1dfe28e7f1e36e3e4a91ecfdb
root <root@ip-10-42-54-169.ec2.internal>
2025-12-01 10:57:52 +0000

    .

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,12 +15,13 @@ size_t count = 0;
 while (h != NULL)
     {
         if (h->str == NULL)
-         printf("[0] (nil)\n");
+        printf("[0] (nil)\n");
 
         else
-        printf("[%d] %s\n", h->len, h->str);
+            printf("[%d] %s\n", h->len, h->str);
+
         count++;
         h = h->next;
-   }
-return (count);
+    }
+    return (count);
 }
