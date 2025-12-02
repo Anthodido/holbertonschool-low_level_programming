0f6553aa339662828117679e6db4d2e3ba3d82ae
root <root@ip-10-42-89-99.ec2.internal>
2025-12-02 08:24:30 +0000

    .

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * list_len - Compte le nombre de noeuds dans une liste de type list_t.
+ * @h: Pointeur constant vers la tete de la liste chainee
+ *
+ * Return: Le nombre de noeuds dans la liste
+ */
 size_t list_len(const list_t *h)
 {
 size_t count = 0;
