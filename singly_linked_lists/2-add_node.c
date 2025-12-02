d40ce0b98e0341db9764a5c1430976ba4f5cb6c7
root <root@ip-10-42-89-99.ec2.internal>
2025-12-02 09:07:06 +0000

    .

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node - Ajoute un nouveau noeud au debut d'une liste list_t.
+ * @head: Pointeur vers le pointeur sur la tete de la liste
+ * @str: Chaine de caracteres a dupliquer dans le nouveau noeud
+ *
+ * Return: L'adresse du nouveau noeud, ou NULL en cas d'echec
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new;
+char *copie;
+unsigned int slen;
+
+if (str == NULL || head == NULL)
+{
+return (NULL);
+}
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+copie = strdup(str);
+if (copie == NULL)
+{
+free(new);
+return (NULL);
+}
+slen = strlen(copie);
+
+new->str = copie;
+new->len = slen;
+new->next = *head;
+*head = new;
+
+return (new);
+}
