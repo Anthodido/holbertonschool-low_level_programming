43867af83f71fdb4ff06b210666495fff509fc5d
root <root@ip-10-42-229-20.ec2.internal>
2025-12-04 08:39:40 +0000

    P1 T6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,24 @@
+#include <stddef.h>
 #include "lists.h"
-
+/**
+ * sum_dlistint - Calcule la somme de tous les n d'une liste dlistint_t.
+ * @head: Pointeur vers le premier noeud de la liste
+ *
+ * Return: La somme de toutes les valeurs n,
+ * ou 0 si la liste est vide
+ */
 int sum_dlistint(dlistint_t *head)
 {
+int sum = 0;
 
+if (head == NULL)
+{
+return (0);
+}
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
